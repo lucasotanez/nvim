98567689468b156cfb52b3736a69ece0a2e366a8
Lucas Otanez <lotanez@usc.edu>
2023-06-28 12:28:25 -0700

    diagnostics, lsp hover, goto definitions, etc

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -63,6 +63,55 @@ return {
           }
         end,
       }
+
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+        callback = function(ev)
+          local opts = { buffer = ev.buf, remap = false, silent = true }
+          vim.keymap.set('n', 'E', function() vim.lsp.buf.hover() end, opts)
+          vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, 
+            { desc = 'Expand vim diagnostic' })
+          vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+
+          vim.keymap.set('n', 'gD', '<cmd>tab split | lua vim.lsp.buf.definition()<CR>',
+            opts)
+          vim.keymap.set('n', '<leader>dp', function() vim.diagnostic.goto_prev() end,
+            opts)
+          vim.keymap.set('n', '<leader>dn', function() vim.diagnostic.goto_next() end,
+            opts)
+          --vim.keymap.set('n', '<leader>r', function()
+          --  local new_name = vim.fn.input { prompt = 'New name: ' }
+          --  if #new_name == 0 then
+          --    return
+          --  end
+          --  vim.lsp.buf.rename(new_name)
+          --end)
+          vim.keymap.set('n', '<leader>ca', function()
+            vim.lsp.buf.code_action {
+              apply = true,
+            }
+          end, opts)
+          vim.keymap.set('n', '<leader>cl', vim.lsp.codelens.run, opts)
+        end,
+      })
+
+      vim.diagnostic.config {
+        virtual_text = true,
+        signs = true,
+        update_in_insert = false,
+        underline = true,
+        severity_sort = true,
+        float = { border = 'rounded' }
+      }
+
+      vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
+        vim.lsp.handlers.hover, {
+          border = 'rounded'
+        })
+      vim.lsp.handlers['textDocuments/signatureHelp'] = vim.lsp.with(
+        vim.lsp.handlers.signature_help, {
+          border = 'rounded',
+        })
     end
   },
 }
