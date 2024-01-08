a9890f305e56012a4393caec1108e93223e47b7b
Lucas Otanez <lotanez@usc.edu>
2024-01-08 09:41:30 -0800

    add formatters for most languages

--- lua/plugins/code.lua
+++ lua/plugins/code.lua
@@ -1,4 +1,46 @@
 return {
-  -- need formatters and stuff can't add rn tho
-
+  {
+    'stevearc/conform.nvim',
+    event = { 'BufWritePre' },
+    cmd = 'ConformInfo',
+    config = function()
+      -- use this directory's stylua.toml if none is found in the current
+      require('conform.formatters.stylua').env = {
+        XDG_CONFIG_HOME = vim.fn.expand('~/.config/nvim/'),
+      }
+      require('conform.formatters.injected').options.ignore_errors = true
+      local util = require('conform.util')
+      local deno_fmt = require('conform.formatters.deno_fmt')
+      local clang_format = require('conform.formatters.clang_format')
+      util.add_formatter_args(deno_fmt, { '--single-quote' }, { append = true })
+      util.add_formatter_args(clang_format, {
+        '--style',
+        '{IndentWidth: 4, AllowShortFunctionsOnASingleLine: Empty}',
+      })
+      require('conform').setup {
+        format_on_save = {
+          timeout_ms = 1000,
+          lsp_fallback = true,
+          quiet = true,
+        },
+        formatters_by_ft = {
+          c = { 'clang_format' },
+          cpp = { 'clang_format' },
+          css = { 'prettierd' },
+          html = { 'prettierd' },
+          java = { 'google-java-format' },
+          javascript = { 'deno_fmt' },
+          javascriptreact = { 'deno_fmt' },
+          json = { 'deno_fmt' },
+          jsonc = { 'deno_fmt' },
+          lua = { 'stylua' },
+          luau = { 'stylua' },
+          markdown = { 'deno_fmt', 'injected' },
+          python = { 'yapf' },
+          typescript = { 'deno_fmt' },
+          typescriptreact = { 'deno_fmt' },
+        },
+      }
+    end,
+  },
 }
